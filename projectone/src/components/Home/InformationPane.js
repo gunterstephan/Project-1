88c746575116e0b90b6bed67fddd1b7c78d31d88
Stephan Gunter <stephangunter.sfg@gmail.com>
2020-09-06 11:43:05 +0200

    cpanel yml

--- projectone/src/components/Home/InformationPane.js
+++ projectone/src/components/Home/InformationPane.js
@@ -4,7 +4,7 @@ class InformationPane extends Component{
     render(){
         return(
             <div className= 'stockprice'>
-                
+                Stock things
             </div>
             
 
